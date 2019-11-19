1a9a96a4048413dc4502083ae1becf7bb3d10d29
Miguel-Dorta <contact@migueldorta.com>
2019-11-19 10:58:58 +0000

    Modify main

--- cmd/web-msg-handler/main.go
+++ cmd/web-msg-handler/main.go
@@ -1,5 +1,4 @@
 package main
-// TODO redo this in general. Also README.md
 
 import (
 	"flag"
@@ -12,59 +11,38 @@ import (
 )
 
 var (
-	configPath   string
-	logFile      string
-	port         int
-	verboseLevel int
-	version      bool
+	configPath string
+	port       int
+	log *logolang.Logger
 )
 
 func init() {
-	flag.StringVar(&configPath, "config", "config.json", "set config path")
-	flag.StringVar(&logFile, "log-file", "", "set log file")
-	flag.IntVar(&port, "port", 8080, "set port")
-	flag.IntVar(&verboseLevel, "verbose", 3, "set verbose level. 0=no-log, 1=critical, 2=errors, 3=info, 4=debug")
-	flag.BoolVar(&version, "version", false, "print version and exit")
+	log = logolang.NewLogger()
+	log.Level = logolang.LevelInfo
 
+	var verbose, version bool
+	flag.StringVar(&configPath, "config", "config.toml", "Path to config file")
+	flag.IntVar(&port, "port", 8080, "Port to listen")
+	flag.BoolVar(&verbose, "verbose", false, "Verbose output")
+	flag.BoolVar(&version, "version", false, "Print version and exit")
 	flag.Parse()
-}
-
-// checkFlags checks if the values assigned to the parsed flags are valid.
-// It will also print version and exit if that flag is set to true.
-func checkFlags() {
-	if port < 0 || port > 65535 {
-		_, _ = fmt.Fprintln(os.Stderr, "invalid port")
-		os.Exit(1)
-	}
-
-	if verboseLevel < 0 || verboseLevel > 4 {
-		_, _ = fmt.Fprintln(os.Stderr, "invalid verbose level")
-		os.Exit(1)
-	}
 
 	if version {
-		_, _ = fmt.Fprintln(os.Stdout, internal.Version)
+		fmt.Println(internal.Version)
 		os.Exit(0)
+	}
+
+	if verbose {
+		log.Level = logolang.LevelDebug
+	}
+
+	if port < 1 || port > 65535 {
+		log.Criticalf("invalid port")
+		os.Exit(1)
 	}
 }
 
 func main() {
-	checkFlags()
-
-	logger := logolang.NewLogger()
-	if logFile != "" {
-		f, err := os.OpenFile(logFile, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
-		if err != nil {
-			_, _ = fmt.Fprintf(os.Stderr, "cannot open log file \"%s\": %s", logFile, err)
-			os.Exit(1)
-		}
-		safeF := &logolang.SafeWriter{W: f}
-		logger = logolang.NewLoggerWriters(safeF, safeF, safeF, safeF)
-	}
-	logger.Color = false
-	logger.Level = verboseLevel
-
-	server.Log = logger
+	server.Log = log
 	server.Run(configPath, strconv.Itoa(port))
 }
-
